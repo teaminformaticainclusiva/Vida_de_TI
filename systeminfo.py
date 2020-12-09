49a701a6f0a08b1cf73fc7dd3be0cfa636e0c786
izaias <zambelestar@gmail.com>
2020-12-08 21:58:02 -0300

    Create systeminfo.py

---
+++ systeminfo.py
@@ -0,0 +1,60 @@
+import psutil
+from datetime import date
+from datetime import datetime
+
+x = datetime.now()
+dias = ('Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo')
+print('\n'"Hoje é", dias[x.weekday()])
+print("Data",x.day,"/", x.month,"/", x.year)
+print("Horas",x.hour ,":",x.minute,":",x.second, '\n')
+
+#By izaias de oliveira elias
+#Fonte de pesquisa(https://pypi.org/project/psutil/)
+
+#Cpu
+cpu_count = psutil.cpu_count()
+cpu_freq = psutil.cpu_freq()
+cpu_percent = psutil.cpu_percent()
+cpu_stats = psutil.cpu_stats()
+cpu_times = psutil.cpu_times()
+cpu_times_percent = psutil.cpu_times_percent()
+#Temperature
+temp = psutil.sensors_temperatures()
+
+#Memory
+mem = psutil.virtual_memory()
+swap = psutil.swap_memory()
+
+#Disck
+disck = psutil.disk_partitions()
+disck_usage = psutil.disk_usage('/')
+
+#Users
+users = psutil.users()
+
+#Time
+time_usage = psutil.boot_time()
+
+#Network
+net = psutil.net_io_counters(pernic=True)
+conect = psutil.net_connections()
+adress_if = psutil.net_if_addrs()
+stats_net = psutil.net_if_stats()
+
+print("Cores:", cpu_count,'\n')
+print("Frequence:", cpu_freq,'\n')
+print("Usage percent:", cpu_percent,'%''\n')
+print("Estate:", cpu_stats,'\n')
+print("Time:", cpu_times,'\n')
+print("Time percent:", cpu_times_percent,'\n')
+print("Temperature", temp,'c°' '\n')
+print("Memory", mem,'\n')
+print("Memory swap", swap,'\n')
+print("Disck mount point", disck,'\n')
+print("Disck usage", disck_usage,'\n')
+print("Users", users,'\n')
+print("Time online", time_usage,'\n')
+print("Network", net,'\n')
+print("Network conect", conect,'\n')
+print("Network adress", adress_if,'\n')
+print("Network stats", stats_net,'\n')
